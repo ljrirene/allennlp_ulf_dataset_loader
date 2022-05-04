4827b2f19340d6664f828a2a39e79a67cae60ac8
ljrirene <34266880+ljrirene@users.noreply.github.com>
2022-05-04 00:53:14 -0400

    Update dataset_reader.py

--- dataset_reader.py
+++ dataset_reader.py
@@ -2,11 +2,11 @@ from allennlp.data import Instance
 from allennlp.data.dataset_readers import DatasetReader
 from allennlp.data.fields import Field, TextField, LabelField, SequenceLabelField, MetadataField
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer
+from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer, SpacyTokenizer
 from typing import Dict, Iterable, List, Iterator
-import csv
 import json
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,22 +14,14 @@ logger = logging.getLogger(__name__)
 class ULFReader(DatasetReader):
     def __init__(
             self,
-            # lazy: bool = False,
             tokenizer: Tokenizer = None,
-            token_indexers: Dict[str, TokenIndexer] = None) -> None:
-        # super().__init__(lazy)
-        self.tokenizer = tokenizer or WhitespaceTokenizer()
-        self.token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+            token_indexers: Dict[str, TokenIndexer] = None,
+            **kwargs):
+        super().__init__(**kwargs)
+        self._tokenizer = tokenizer or SpacyTokenizer()
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
 
-    def _read(self, file_path: str) -> Iterable[Instance]:
-        with open(file_path) as dataset_file:
-            lines = json.load(dataset_file)
-        logging.info("Reading entries from lines in file at %s", file_path)
-        for line in lines:
-            _, sentence, ulf, ulf_amr = line[0], line[1], line[2], line[3]
-            yield self.text_to_instance(sentence, ulf, ulf_amr)
-
-    def text_to_instance(self, sentence: str, ulf: str, ulf_amr: str = None) -> Instance:
+    def text_to_instance(self, sentence: str, ulf: str, ulf_amr: str) -> Instance:
         tokens = self.tokenizer.tokenize(sentence)
         text_field = TextField(tokens, self.token_indexers)
         fields: Dict[str, Field] = {"text": text_field}
@@ -39,9 +31,16 @@ class ULFReader(DatasetReader):
             fields["ulf_amr"] = LabelField(ulf_amr)
         return Instance(fields)
 
+    def _read(self, file_path):
+        print("in")
+        with open(file_path) as f:
+            lines = json.load(f)
+        logging.info("Reading entries from lines in file at %s", file_path)
+        for line in lines:
+            _, sentence, ulf, ulf_amr = line[0], line[1], line[2], line[3]
+            yield self.text_to_instance(sentence, ulf, ulf_amr)
+
 
 if __name__ == '__main__':
-
-    dataset_reader = ULFReader()
-    #dataset_reader.read("test.json")
-
+    reader = ULFReader()
+    reader.read("./ulf-data-1.0/ulf/dev/all.json")
