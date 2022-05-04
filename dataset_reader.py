81fc3fa32e76ca203e694cdbad80496a2ff8f6ea
ljrirene <34266880+ljrirene@users.noreply.github.com>
2022-05-04 01:22:57 -0400

    Update dataset_reader.py

--- dataset_reader.py
+++ dataset_reader.py
@@ -1,12 +1,11 @@
-from allennlp.data import Instance
-from allennlp.data.dataset_readers import DatasetReader
-from allennlp.data.fields import Field, TextField, LabelField, SequenceLabelField, MetadataField
+from typing import Dict, Iterable, List
+
+from allennlp.data import DatasetReader, Instance
+from allennlp.data.fields import Field, LabelField, TextField
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
-from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer, SpacyTokenizer
-from typing import Dict, Iterable, List, Iterator
+from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer
 import json
 import logging
-
 logger = logging.getLogger(__name__)
 
 
@@ -18,8 +17,16 @@ class ULFReader(DatasetReader):
             token_indexers: Dict[str, TokenIndexer] = None,
             **kwargs):
         super().__init__(**kwargs)
-        self._tokenizer = tokenizer or SpacyTokenizer()
-        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+        self.tokenizer = tokenizer or WhitespaceTokenizer()
+        self.token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+
+    def _read(self, file_path):
+        with open(file_path) as f:
+            lines = json.load(f)
+        logging.info("Reading entries from lines in file at %s", file_path)
+        for line in lines:
+            _, sentence, ulf, ulf_amr = line[0], line[1], line[2], line[3]
+            yield self.text_to_instance(sentence, ulf, ulf_amr)
 
     def text_to_instance(self, sentence: str, ulf: str, ulf_amr: str) -> Instance:
         tokens = self.tokenizer.tokenize(sentence)
@@ -31,16 +38,10 @@ class ULFReader(DatasetReader):
             fields["ulf_amr"] = LabelField(ulf_amr)
         return Instance(fields)
 
-    def _read(self, file_path):
-        print("in")
-        with open(file_path) as f:
-            lines = json.load(f)
-        logging.info("Reading entries from lines in file at %s", file_path)
-        for line in lines:
-            _, sentence, ulf, ulf_amr = line[0], line[1], line[2], line[3]
-            yield self.text_to_instance(sentence, ulf, ulf_amr)
-
 
 if __name__ == '__main__':
-    reader = ULFReader()
-    reader.read("./ulf-data-1.0/ulf/dev/all.json")
+    dataset_reader = ULFReader()
+    instances = list(dataset_reader.read("./ulf-data-1.0/ulf/dev/all.json"))
+
+    for instance in instances[:1]:
+        print(instance)
